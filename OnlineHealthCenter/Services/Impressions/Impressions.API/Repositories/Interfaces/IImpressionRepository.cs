6de3d4265e26e625a64cdd3c62b0aa651955ef5a
Sara Matic <smatic.97@gmail.com>
2023-01-12 21:43:55 +0100

    Impressions.API - Repository and Controller improvements

--- OnlineHealthCenter/Services/Impressions/Impressions.API/Repositories/Interfaces/IImpressionRepository.cs
+++ OnlineHealthCenter/Services/Impressions/Impressions.API/Repositories/Interfaces/IImpressionRepository.cs
@@ -5,5 +5,12 @@ namespace Impression.API.Repositories.Interfaces
     public interface IImpressionRepository
     {
         Task<IEnumerable<PatientReview>> GetImpressions();
+        Task<PatientReview> GetImpressionById(string id);
+        Task<IEnumerable<PatientReview>> GetImpressionsByDoctorId(string id);
+        Task<IEnumerable<PatientReview>> GetImpressionsByPatientId(string id);
+        Task AddImpression(PatientReview review);
+        Task<bool> UpdateImpression(PatientReview review);
+        Task<bool> DeleteImpression(string patientId, string doctorId);
+        Task<decimal> GetMark(string doctorId);
     }
 }
