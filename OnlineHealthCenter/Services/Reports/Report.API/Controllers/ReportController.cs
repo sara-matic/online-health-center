3cf4d1b01550b78d45b849ebbc094359af3a64df
anapantic <ana.pantic34@gmail.com>
2023-01-23 04:36:38 +0100

    IdentityServer - added JWT security to Reports.API
    Closes #47

--- OnlineHealthCenter/Services/Reports/Report.API/Controllers/ReportController.cs
+++ OnlineHealthCenter/Services/Reports/Report.API/Controllers/ReportController.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Reports.Common.DTOs;
 using Reports.Common.Entities;
 using Reports.Common.Repositories.Interfaces;
+using System.Security.Claims;
 
 namespace Reports.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/v1/[controller]")]
     public class ReportController : ControllerBase
@@ -19,45 +22,76 @@ namespace Reports.API.Controllers
             this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
+        [Authorize(Roles = "Patient")]
         [Route("[action]/{patientId}")]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<ReportDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<ReportDTO>>> GetReportsByPatientId(string patientId)
         {
+            if (User.FindFirst(ClaimTypes.NameIdentifier).Value != patientId)
+            {
+                return Forbid();
+            }
+
             var reports = await this.repository.GetReportsByPatientId(patientId);
             return reports == null || !reports.Any() ? NotFound() : Ok(mapper.Map<IEnumerable<ReportDTO>>(reports));
         }
 
+        [Authorize(Roles = "Doctor")]
+        [Authorize(Roles = "Nurse")]
         [Route("[action]/{doctorId}")]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<ReportDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<ReportDTO>>> GetReportsByDoctorId(string doctorId)
         {
+            if (User.FindFirst(ClaimTypes.Role).Value == "Doctor" && User.FindFirst(ClaimTypes.NameIdentifier).Value != doctorId)
+            {
+                return Forbid();
+            } 
+
             var reports = await this.repository.GetReportsByDoctorId(doctorId);
             return reports == null || !reports.Any() ? NotFound() : Ok(mapper.Map<IEnumerable<ReportDTO>>(reports));
         }
 
+        [Authorize(Roles = "Patient")]
+        [Authorize(Roles = "Doctor")]
         [Route("[action]/{doctorId}/{patientId}")]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<ReportDTO>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<ReportDTO>>> GetReportsByPatientAndDoctorId(string patientId, string doctorId)
         {
+            if (User.FindFirst(ClaimTypes.Role).Value == "Patient" && User.FindFirst(ClaimTypes.NameIdentifier).Value != patientId)
+            {
+                return Forbid();
+            } 
+            else if (User.FindFirst(ClaimTypes.Role).Value == "Doctor" && User.FindFirst(ClaimTypes.NameIdentifier).Value != doctorId)
+            {
+                return Forbid();
+            }
+
             var reports = await this.repository.GetReportsByPatientAndDoctorId(patientId, doctorId);
             return reports == null || !reports.Any() ? NotFound() : Ok(mapper.Map<IEnumerable<ReportDTO>>(reports));
         }
 
+        [Authorize(Roles = "Doctor")]
         [Route("[action]")]
         [HttpPost]
         [ProducesResponseType(typeof(void), StatusCodes.Status201Created)]
         public async Task<IActionResult> CreateReport([FromBody] CreateReportDTO createReportDTO)
         {
+            if (User.FindFirst(ClaimTypes.NameIdentifier).Value != createReportDTO.DoctorId)
+            {
+                return Forbid();
+            }
+
             await this.repository.CreateReport(this.mapper.Map<Report>(createReportDTO));
             return Ok();
         }
 
+        [Authorize(Roles = "Nurse")]
         [Route("[action]")]
         [HttpDelete]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
@@ -67,7 +101,9 @@ namespace Reports.API.Controllers
             var report = await this.repository.GetReportByIdAndTime(patientId, doctorId, createdTime);
 
             if (report == null)
+            {
                 return BadRequest();
+            }
 
             return Ok(await this.repository.DeleteReport(report.Id));
         }
--- OnlineHealthCenter/Services/Reports/Reports.Common/Extensions/ReportExtensions.cs
+++ OnlineHealthCenter/Services/Reports/Reports.Common/Extensions/ReportExtensions.cs
@@ -3,6 +3,10 @@ using Reports.Common.Repositories;
 using Reports.Common.Data;
 using Reports.Common.Repositories.Interfaces;
 using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Reports.Common.Extensions
 {
@@ -14,6 +18,34 @@ namespace Reports.Common.Extensions
             services.AddScoped<IReportRepository, ReportRepository>();
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+
+        public static IServiceCollection ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings.GetSection("secretKey").Value;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = jwtSettings.GetSection("validIssuer").Value,
+                        ValidAudience = jwtSettings.GetSection("validAudience").Value,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                    };
+                });
+
+            return services;
         }
     }
 }
