517a9e5daf312fc9211fc5c6efb5b07c782d2dd6
Sara Matic <smatic.97@gmail.com>
2023-05-10 21:20:12 +0200

    WebApp: Created clinic information component
    #100

---
+++ OnlineHealthCenter/WebApps/OnlineHealthCenterSPA/src/app/employee-information/feature-clinic-information/clinic-information/clinic-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clinic-information',
+  templateUrl: './clinic-information.component.html',
+  styleUrls: ['./clinic-information.component.css']
+})
+export class ClinicInformationComponent {
+
+}
